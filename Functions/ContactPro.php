1d146ec32a9f6a909f34667d47e65cdcaa81bc93
Shirin Vazirian <Shirin1268@gmail.com>
2018-05-23 15:56:34 +0200

    this is the problem

--- Functions/ContactPro.php
+++ Functions/ContactPro.php
@@ -1,29 +1,10 @@
 <?php
-//require_once('autoload.php');
-
-
 /*
     if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){
-$sitekey = '6LerzlkUAAAAAN_sVuSAuvU7sSMbbnaN-RfbJMEj';
 $secretkey = '6LerzlkUAAAAAHIz4bJMdA4u6-GJNnrGuNiVG0AC';
    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
           $responseData = json_decode($verifyResponse);
           if($responseData->success)*/
-          /*
-$RECAPTCHA = NEW \RECAPTCHA\RECAPTCHA($secretkey);
- 
-$grecaptcharesponse = $_POST['G-RECAPTCHA-RESPONSE']; //GOOGLE CAPTCHA POST DATA
-$remotip = $_SERVER['REMOTE_ADDR']; //TO GET USER'S IP
- 
-$recaptchaerrors = ''; // BLANK VARIBLE TO STORE ERROR
- 
-$resp = $RECAPTCHA->VERIFY($grecaptcharesponse, $remotip); //METHOD TO VERIFY CAPTCHA
-IF ($resp->ISSUCCESS()){*/
-  //get verify response data
-        
-
- 
-   // SEND MAIL OR INSERT IN DB OR DO WHATVER YOU WISH TO
 
  if ($_POST['submit']){
     if(isset($_POST['email'])) 
